9c1ce95f2f5aa1883afed3ee0c81200c732fbc9a
shishi42 <benriv4272@gmail.com>
2020-06-26 03:14:56 +0200

    re fix

--- commands/uptime.js
+++ commands/uptime.js
@@ -6,8 +6,8 @@ module.exports.run = async (bot, message, args) => {
   function duration(ms){
     const sec = Math.floor((ms / 1000) % 60).toString()
     const min = Math.floor((ms / (1000 * 60)) % 60).toString()
-    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
-    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 24).toString()
+    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 24).toString()
+    const days = Math.floor((ms / (1000 * 60 * 60 * 24))).toString()
 
     return `${days.padStart(1, '0')} jours, ${hrs.padStart(2, '0')} heures, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} secondes`
   }

02ec163331e26ef4f254b51963adc08d41145c73
shishi42 <benriv4272@gmail.com>
2021-02-08 10:47:04 +0100

    re-update

--- commands/addEvent.js
+++ commands/addEvent.js
@@ -2,10 +2,12 @@ const Discord = require("discord.js")
 const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
+  args_str = args.slice(1).join(" ")
+  message.channel.send("Adding event : **"+args_str+"**, at the date : **"+args[0]+"**")
+  message.delete()
 }
 
 module.exports.config = {
   name: "addEvent",
-  aliases: ["addEvent"]
-
+  aliases: ["adev"]
 }

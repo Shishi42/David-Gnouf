ec8edf5469bf7e57bfacc093cb5be930f38e71b4
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 12:56:17 +0100

    Update addEvent.js

--- commands/addEvent.js
+++ commands/addEvent.js
@@ -2,6 +2,16 @@ const Discord = require("discord.js")
 const config = require("../config.json")
 
 module.exports.run = async (bot, message, args) => {
+      
+  let messageArray = message.content.split(" ")
+    
+  date = messageArray[1]
+  message = args.slice(1).join(' ')
+    
+  if(bot.eventjson[date] === undefined) bot.eventjson[date] = new Array()
+  bot.eventjson[date].push(msg)
+  fs.writeFile("./event.json", JSON.stringify(bot.eventjson, null, 4), function(err) {})
+   
   args_str = args.slice(1).join(" ")
   message.channel.send("Adding event : **"+args_str+"**, at the date : **"+args[0]+"**")
   message.delete()

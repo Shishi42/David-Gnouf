f497673846b706d398ed7613d928efa1512d8ed8
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 12:46:21 +0100

    Update listEvent.js

--- commands/listEvent.js
+++ commands/listEvent.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
-const config = require("../config.json");
+const config = require("../config.json")
+
 const fs = require("fs");
 const jsonfile = require ("jsonfile")
 

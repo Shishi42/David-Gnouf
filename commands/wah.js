22cc9305b23356f16c7110d90db6564668d1f404
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-05-10 18:21:17 +0200

    Update wah.js

--- commands/wah.js
+++ commands/wah.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const config = require("../config.json")
 
 module.exports.run = async (bot, message, args) => {
-  message.channel.send(':www::aaa::hhh:')
+  message.channel.send('<:www:841346022885294140><:aaa:841346022885294140><:hhh:841346022885294140>')
   return message.delete()
 }
 

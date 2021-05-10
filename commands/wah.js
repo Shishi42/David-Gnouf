81653fe6ab0c52861bdd5bd2de9c3d77f83a5d89
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-05-10 18:08:07 +0200

    Create wah.js

---
+++ commands/wah.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const config = require("../config.json")
+
+module.exports.run = async (bot, message, args) => {
+  message.channel.send(':www: :aaa: :hhh:')
+  return message.delete()
+}
+
+module.exports.config = {
+  name: "wah",
+  aliases: [],
+  args: [],
+  usage: ["wah"],
+  desc: "Send wah."
+}

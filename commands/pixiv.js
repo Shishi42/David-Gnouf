3c3bee9a5a4945a27aa76accd940a0e3ea0b9862
shishi42 <benji.riviere72@hotmail.fr>
2022-04-20 15:57:38 +0200

    pixiv

---
+++ commands/pixiv.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+const config = require("../config.json")
+
+module.exports.run = async (bot, message, args) => {
+
+  args.forEach((arg) => {
+    if(arg.startsWith("u")) message.channel.send("https://www.pixiv.net/en/users/"+arg.split("u")[1])
+    else message.channel.send("https://www.pixiv.net/en/artworks/"+arg)
+  })
+}
+
+module.exports.config = {
+  name: "pixiv",
+  aliases: ["pix","p"],
+  args: ["[artwork_id OR u<user_id>]"],
+  usage: ["pixiv [artwork_id OR u<user_id>]"],
+  desc: "Send pixiv artwork or user link with your ids."
+}

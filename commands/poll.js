39207678884ded44f7677bc33a85c76508db018d
Shishi <37541779+Shishi42@users.noreply.github.com>
2023-09-29 22:32:22 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -26,7 +26,7 @@ module.exports = {
         msg.react("<:check:334046325098414080>")
         msg.react("❔")
         msg.react("❌")})
-      // .catch((error) => {message.reply("Message not found with this id.")})
+      .catch((error) => {console.log(error)})
     return message.editReply({content: "Done.", ephemeral: true})
   }
 }

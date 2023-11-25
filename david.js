3e35e357b7af7a7a6c26dbb44b02ed2181cbb69c
shishi42 <benji.riviere72@hotmail.fr>
2023-11-25 01:35:10 +0100

    adding database

--- david.js
+++ david.js
@@ -10,6 +10,8 @@ const jsonfile = require ("jsonfile")
 const fs = require("fs")
 const { Player } = require("discord-player")
 
+const Sequelize = require("sequelize")
+
 bot.commands = new Discord.Collection()
 bot.eventjson = {}
 bot.color = "553380"
@@ -23,6 +25,148 @@ bot.player.extractors.loadDefault()
 
 bot.on("ready", async () => {
   console.log(`Connected as ${bot.user.tag}!`)
+
+  bot.db = new Sequelize({
+    dialect: "sqlite",
+    storage: "./sleep.db"
+  })
+
+  bot.Pokemons = bot.db.define("pokemon", {
+    pokemon_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    pokemon_nom: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    pokemon_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    type_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    skill_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    pokemon_tier: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  })
+
+  bot.Types = bot.db.define("type", {
+    type_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    type_nom: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    type_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  })
+
+  bot.Skills = bot.db.define("skill", {
+    skill_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    skill_nom: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    skill_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    skill_tier_early: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    skill_tier_late: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  })
+
+  bot.Subskills = bot.db.define("subskill", {
+    subskill_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    subskill_nom: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    subskill_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  })
+
+  bot.Natures = bot.db.define("natures", {
+    nature_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    nature_nom: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    nature_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    nature_incr_fr: {
+      type: Sequelize.STRING,
+    },
+    nature_incr_en: {
+      type: Sequelize.STRING,
+    },
+    nature_decr_fr: {
+      type: Sequelize.STRING,
+    },
+    nature_decr_en: {
+      type: Sequelize.STRING,
+    },
+  })
+
+  await bot.Pokemons.sync()
+  await bot.Types.sync()
+  await bot.Skills.sync()
+  await bot.Subskills.sync()
+  await bot.Natures.sync()
+
+  if(!await bot.Pokemons.findOne()) await require("./db_load.js").run(bot)
+
+  console.log("Database Online !")
 
   await slashcommands_loader(bot)
 
---
+++ db_load.js
@@ -0,0 +1,262 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+  async run(bot) {
+
+    dict = {
+
+    }
+
+    skills = [
+      ["Dream Shard Magnet S","Aimant à Fragment de Rêve S","-2","-2"],
+      ["Dream Shard Magnet S - Var","Aimant à Fragment de Rêve S - Var","-2","-2"],
+      ["Ingredient Magnet S","Aiment à Ingrédient S","1","0"],
+      ["Charge Energy S","Charge d'Énergies S","0","1"],
+      ["Charge Strength M","Charge de Puissance M","1","1"],
+      ["Charge Strength S","Charge de Puissance S","-1","-1"],
+      ["Charge Strength S - Var","Charge de Puissance S - Var","0","0"],
+      ["Energizing Cheer S","Encouragement Énergique S","-1","0"],
+      ["Energy for Everyone S","Énergie Partagée S","2","2"],
+      ["Cooking Power-Up S","Garde-Manger S","-1","1"],
+      ["Metronome","Métronome","0","0"],
+      ["Extra Helpful S","Soutien Extra S","-1","-1"]
+    ]
+
+    subskills = [
+      ["Berry Finding S","Fan de Baies S"],
+      ["Helping Speed S","Soutien Rapide S"],
+      ["Helping Speed M","Soutien Rapide M"],
+      ["Ingredient Finder S","Ingrédients + S"],
+      ["Ingredient Finder M","Ingrédients + M"],
+      ["Inventory Up S","Inventaire + S"],
+      ["Inventory Up M","Inventaire + M"],
+      ["Inventory Up L","Inventaire + L"],
+      ["Skill Level Up S","Niveau Compétence + S"],
+      ["Skill Level Up M","Niveau Compétence + M"],
+      ["Skill Trigger S","Compétence + S"],
+      ["Skill Trigger M","Compétence + M"],
+      ["Dream Shard Bonus","Bonus Fragment de Rêve"],
+      ["Energy Recovery Bonus","Bonus Récup'Énergie"],
+      ["Helping Bonus","Bonus Soutien"],
+      ["Research EXP Bonus","Bonus Point de recherche"],
+      ["Sleep EXP Bonus","Bonus Dodo"],
+      ["Energy Recovery S","Récup'Énergie + S"],
+      ["Energy Recovery M","Récup'Énergie + M"]
+    ]
+
+    natures = [
+      ["Assuré","Bold","Energy Recovery,Speed of Help","Récup. Énergie,Vitesse Soutien"],
+      ["Bizarre","Quirky","",""],
+      ["Brave","Brave","Speed of Help,EXP Gains","Vitesse Soutien,EXP Obtenue"],
+      ["Calme","Calm","Main Skill Chance,Speed of Help","% Comp.,Vitesse Soutien"],
+      ["Discret","Quiet","Ingredient Finding,EXP Gains","% Ingrédients,EXP Obtenue"],
+      ["Docile","Docile","",""],
+      ["Doux","Mild","Ingredient Finding,Energy Recovery","% Ingrédients,Récup. Énergie"],
+      ["Foufou","Rash","Ingredient Finding,Main Skill Chance","% Ingrédients,% Comp."],
+      ["Gentil","Gentle","Main Skill Chance,Energy Recovery","% Comp.,Récup. Énergie"],
+      ["Hardi","Hardy","",""],
+      ["Jovial","Jolly","EXP Gains,Ingredient Finding","EXP Obtenue,% Ingrédients"],
+      ["Lâche","Lax","Energy Recovery,Main Skill Chance","Récup. Énergie,% Comp."],
+      ["Malin","Impish","Energy Recovery,Ingredient Finding","Récup. Énergie,% Ingrédients"],
+      ["Malpoli","Sassy","Main Skill Chance,EXP Gains","% Comp.,EXP Obtenue"],
+      ["Mauvais","Naughty","Speed of Help,Main Skill Chance","Vitesse Soutien,% Comp."],
+      ["Modeste","Modest","Ingredient Finding,Speed of Help","% Ingrédients,Vitesse Soutien"],
+      ["Naïf","Naive","EXP Gains,Main Skill Chance","EXP Obtenue,% Comp."],
+      ["Pressé","Hasty","EXP Gains,Energy Recovery","EXP Obtenue,Récup. Énergie"],
+      ["Prudent","Careful","Main Skill Chance,Ingredient Finding","% Comp.,% Ingrédients"],
+      ["Pudique","Bashful","",""],
+      ["Relax","Relaxed","Energy Recovery,EXP Gains","Récup. Énergie,EXP Obtenue"],
+      ["Rigide","Adamant","Speed of Help,Ingredient Finding","Vitesse Soutien,% Ingrédients"],
+      ["Sérieux","Serious","",""],
+      ["Solo","Lonely","Speed of Help,Energy Recovery","Vitesse Soutien,Récup. Énergie"],
+      ["Timide","Timid","EXP Gains,Speed of Help","EXP Obtenue,Vitesse Soutien"]
+    ]
+
+    types = [
+      ["Berries","Baies"],
+      ["Ingredients","Ingrédients"],
+      ["Skills","Compétences"]
+    ]
+
+    pokemon1 = [
+      ["Bulbizarre","Ingrédients","Aiment à Ingrédient S","C+"],
+      ["Herbizarre","Ingrédients","Aiment à Ingrédient S","B"],
+      ["Florizarre","Ingrédients","Aiment à Ingrédient S","A-"],
+      ["Salamèche","Ingrédients","Aiment à Ingrédient S","B-"],
+      ["Reptincel","Ingrédients","Aiment à Ingrédient S","B+"],
+      ["Dracaufeu","Ingrédients","Aiment à Ingrédient S","A+"],
+      ["Carapuce","Ingrédients","Aiment à Ingrédient S","C+"],
+      ["Carabaffe","Ingrédients","Aiment à Ingrédient S","B"],
+      ["Tortank","Ingrédients","Aiment à Ingrédient S","A-"],
+      ["Chenipan","Baies","Aiment à Ingrédient S","C+"],
+      ["Chrysacier","Baies","Aiment à Ingrédient S","B-"],
+      ["Papilusion","Baies","Aiment à Ingrédient S","S-"],
+      ["Rattata","Baies","Charge d'Énergies S","C"],
+      ["Rattatac","Baies","Charge d'Énergies S","A-"],
+      ["Abo","Baies","Charge d'Énergies S","C"],
+      ["Arbok","Baies","Charge d'Énergies S","B"],
+      ["Pichu","Baies","Charge de Puissance S","C+"],
+      ["Pikachu","Baies","Charge de Puissance S","A"],
+      ["Raichu","Baies","Charge de Puissance S","S+"],
+      ["Mélo","Baies","Métronome","C-"],
+      ["Mélofée","Baies","Métronome","B-"],
+      ["Mélodelfe","Baies","Métronome","A"],
+      ["Toudoudou","Compétences","Énergie Partagée S","C-"],
+      ["Rondoudou","Compétences","Énergie Partagée S","B-"],
+      ["Grodoudou","Compétences","Énergie Partagée S","S"],
+      ["Taupiqueur","Ingrédients","Charge de Puissance S","C+"],
+      ["Triopikeur","Ingrédients","Charge de Puissance S","A"],
+      ["Miaouss","Compétences","Aimant à Fragment de Rêve S","F"],
+      ["Persian","Compétences","Aimant à Fragment de Rêve S","B"],
+      ["Psykokwak","Compétences","Charge de Puissance S - Var","F"],
+      ["Akwakwak","Compétences","Charge de Puissance S - Var","B"],
+      ["Férosinge","Baies","Charge de Puissance S - Var","B-"],
+      ["Colossinge","Baies","Charge de Puissance S - Var","A"],
+      ["Caninos","Compétences","Soutien Extra S","C"],
+      ["Arcanin","Compétences","Soutien Extra S","A"],
+      ["Chétiflor","Ingrédients","Charge d'Énergies S","C-"],
+      ["Boustiflor","Ingrédients","Charge d'Énergies S","B-"],
+      ["Empiflor","Ingrédients","Charge d'Énergies S","A"],
+      ["Racaillou","Ingrédients","Charge de Puissance S - Var","F"],
+      ["Gravalanch","Ingrédients","Charge de Puissance S - Var","B-"],
+      ["Grolem","Ingrédients","Charge de Puissance S - Var","B+"],
+      ["Ramoloss","Compétences","Encouragement Énergique S","F"],
+      ["Flagadoss","Compétences","Encouragement Énergique S","B+"],
+      ["Roigada","Compétences","Encouragement Énergique S","A-"],
+      ["Magnéti","Compétences","Garde-Manger S","F"],
+      ["Magnéton","Compétences","Garde-Manger S","B-"],
+      ["Magnézone","Compétences","Garde-Manger S","A"],
+      ["Doduo","Baies","Charge d'Énergies S","B"],
+      ["Dodrio","Baies","Charge d'Énergies S","S+"],
+      ["Fantominus","Ingrédients","Charge de Puissance S","B"],
+      ["Spectrum","Ingrédients","Charge de Puissance S","A"],
+      ["Ectoplasma","Ingrédients","Charge de Puissance S","S+"],
+      ["Onix","Baies","Aiment à Ingrédient S","A-"],
+      ["Steelix","Baies","Aiment à Ingrédient S","A"],
+      ["Osselait","Baies","Charge d'Énergies S","C"],
+      ["Ossatueur","Baies","Charge d'Énergies S","B+"],
+      ["Kangourex","Ingrédients","Aiment à Ingrédient S","A-"],
+      ["Mime Jr.","Ingrédients","Charge de Puissance S","C+"],
+      ["M. Mime","Ingrédients","Charge de Puissance S","A"],
+      ["Scarabrute","Ingrédients","Charge de Puissance S","A"],
+      ["Métamorph","Ingrédients","Charge de Puissance S","A"],
+      ["Évoli","Compétences","Aiment à Ingrédient S","C"],
+      ["Aquali","Compétences","Aiment à Ingrédient S","B"],
+      ["Voltali","Compétences","Soutien Extra S","A"],
+      ["Pyroli","Compétences","Garde-Manger S","S-"],
+      ["Mentali","Compétences","Charge de Puissance M","S-"],
+      ["Noctali","Compétences","Charge d'Énergies S","B-"],
+      ["Phyllali","Compétences","Encouragement Énergique S","B"],
+      ["Givrali","Compétences","Garde-Manger S","B+"],
+      ["Nymphali","Compétences","Énergie Partagée S","S-"],
+      ["Germignon","Baies","Charge de Puissance S - Var","B-"],
+      ["Macronium","Baies","Charge de Puissance S - Var","B+"],
+      ["Méganium","Baies","Charge de Puissance S - Var","A+"],
+      ["Héricendre","Baies","Charge de Puissance S - Var","B"],
+      ["Feurisson","Baies","Charge de Puissance S - Var","A-"],
+      ["Typhlosion","Baies","Charge de Puissance S - Var","S"],
+      ["Kaiminus","Baies","Charge de Puissance S - Var","C+"],
+      ["Crocrodil","Baies","Charge de Puissance S - Var","B"],
+      ["Aligatueur","Baies","Charge de Puissance S - Var","A"],
+      ["Togepi","Compétences","Métronome","C-"],
+      ["Togetic","Compétences","Métronome","C+"],
+      ["Togekiss","Compétences","Métronome","A-"],
+      ["Wattouat","Compétences","Charge de Puissance M","C+"],
+      ["Lainergie","Compétences","Charge de Puissance M","B+"],
+      ["Pharamp","Compétences","Charge de Puissance M","S"],
+      ["Manzaï","Compétences","Charge de Puissance M","F"],
+      ["Simularbre","Compétences","Charge de Puissance M","B-"],
+      ["Okéoké","Compétences","Encouragement Énergique S","F"],
+      ["Qulbutoké","Compétences","Encouragement Énergique S","B-"],
+      ["Scarhino","Baies","Aiment à Ingrédient S","A-"],
+      ["Malosse","Baies","Charge de Puissance S","C"],
+      ["Démolosse","Ingrédients","Charge de Puissance S","B+"],
+      ["Embrylex","Ingrédients","Charge d'Énergies S","C"],
+      ["Ymphect","Ingrédients","Charge d'Énergies S","B-"],
+      ["Tyranocif","Baies","Charge d'Énergies S","A+"],
+      ["Parecool","Baies","Aiment à Ingrédient S","C"],
+      ["Vigoroth","Baies","Aiment à Ingrédient S","A-"],
+      ["Monaflèmit","Compétences","Aiment à Ingrédient S","A-"],
+      ["Ténéfix","Compétences","Aimant à Fragment de Rêve S - Var","C+"],
+      ["Gloupti","Compétences","Aimant à Fragment de Rêve S - Var","F"],
+      ["Avaltout","Baies","Aimant à Fragment de Rêve S - Var","C+"],
+      ["Tylton","Baies","Charge d'Énergies S","B-"],
+      ["Altaria","Baies","Charge d'Énergies S","B+"],
+      ["Polichombr","Baies","Charge de Puissance S","B-"],
+      ["Branette","Ingrédients","Charge de Puissance S","S-"],
+      ["Absol","Baies","Charge de Puissance S","S-"],
+      ["Obalie","Baies","Aiment à Ingrédient S","C-"],
+      ["Phogleur","Baies","Aiment à Ingrédient S","B-"],
+      ["Kaimorse","Compétences","Aiment à Ingrédient S","A"],
+      ["Riolu","Compétences","Aimant à Fragment de Rêve S","C"],
+      ["Lucario","Ingrédients","Aimant à Fragment de Rêve S","B+"],
+      ["Cradopaud","Ingrédients","Charge de Puissance S","C-"],
+      ["Coatox","Ingrédients","Charge de Puissance S","B+"]
+    ]
+
+    pokemon2 = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Rattata","Raticate","Ekans","Arbok","Pichu","Pikachu","Raichu","Cleffa","Clefairy","Clefable","Igglybuff","Jigglypuff","Wigglytuff","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Bellsprout","Weepinbell","Victreebel","Geodude","Graveler","Golem","Slowpoke","Slowbro","Slowking","Magnemite","Magneton","Magnezone","Doduo","Dodrio","Gastly","Haunter","Gengar","Onix","Steelix","Cubone","Marowak","Kangaskhan","Mime Jr.","Mr. Mime","Pinsir","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Espeon","Umbreon","Leafeon","Glaceon","Sylveon","Chikorita","Bayleef","Meganium","Cyndaquil","Quilava","Typhlosion","Totodile","Croconaw","Feraligatr","Togepi","Togetic","Togekiss","Mareep","Flaaffy","Ampharos","Bonsly","Sudowoodo","Wynaut","Wobbuffet","Heracross","Houndour","Houndoom","Larvitar","Pupitar","Tyranitar","Slakoth","Vigoroth","Slaking","Sableye","Gulpin","Swalot","Swablu","Altaria","Shuppet","Banette","Absol","Spheal","Sealeo","Walrein","Riolu","Lucario","Croagunk","Toxicroak"]
+
+    skills.forEach((item, i) => {
+      bot.Skills.create({
+        skill_id: i,
+        skill_nom: item[1],
+        skill_name: item[0],
+        skill_tier_early: item[2],
+        skill_tier_late: item[3],
+      })
+    })
+
+    subskills.forEach((item, i) => {
+      bot.Subskills.create({
+        subskill_id: i,
+        subskill_nom: item[1],
+        subskill_name: item[0],
+      })
+    })
+
+    natures.forEach((item, i) => {
+
+      if(item[2] == ""){
+        fr = ["",""]
+        en = ["",""]
+      } else {
+        fr = item[3].split(',')
+        en = item[2].split(',')
+      }
+
+      bot.Natures.create({
+        nature_id: i,
+        nature_nom: item[0],
+        nature_name: item[1],
+        nature_incr_fr: fr[0],
+        nature_incr_en: en[0],
+        nature_decr_fr: fr[1],
+        nature_decr_en: en[1],
+      })
+    })
+
+    types.forEach((item, i) => {
+      bot.Types.create({
+        type_id: i,
+        type_nom: item[0],
+        type_name: item[1],
+      })
+    })
+
+    pokemon1.forEach((item, i) => {
+
+      type_id = types.findIndex(x => x.includes(item[1]))
+      skill_id = skills.findIndex(x => x.includes(item[2]))
+
+      bot.Pokemons.create({
+        pokemon_id: i,
+        pokemon_nom: item[0],
+        pokemon_name: pokemon2[i],
+        type_id: type_id,
+        skill_id: skill_id,
+        pokemon_tier: item[3],
+      })
+    })
+  }
+}
